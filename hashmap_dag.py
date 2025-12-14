e323518b2b350a2c438f342456b282701cb6f801
= <2mmanikan@gmail.com>
2025-12-13 22:07:36 -0800

    added data flow graph traversal to the operation flow graph traversal. Added name capability as well

--- hashmap_dag.py
+++ hashmap_dag.py
@@ -15,13 +15,13 @@ class Hashmap:
         self.mapping = {} #pair: (tensor, name)
     
     def get_name(self, t):
-        return self.mapping[t]
+        return self.mapping[(t)]
     
     def add(self, t, name):
-        self.mapping[t] = name
+        self.mapping[(t)] = name
     
     def remove(self, t):
-        return self.mapping.pop(t)
+        return self.mapping.pop((t))
 
     def display(self):
         print("-------------------")
@@ -36,16 +36,22 @@ class Tensor_Graph:
         self.edges = {} #(result_tensor_id:[original tensor id's])
     
     def add_tensor_edge(self, t1, t2):
-        if id(t1) not in self.edges.keys():
-           self.edges[id(t1)] = [id(t2)]
+        if (t1) not in self.edges.keys():
+           self.edges[(t1)] = [(t2)]
         else:
-           self.edges[id(t1)].append(id(t2)) 
+           self.edges[(t1)].append((t2)) 
     
     def remove_tensor_edge(self, t1):
-        return self.edges.pop(id(t1))
+        return self.edges.pop((t1))
+    
+    def get_contributing_tensors(self, t):
+        if (t) in self.edges.keys():
+           return self.edges[(t)]
+        else:
+           return []
     
 tensor_graph = Tensor_Graph()
-
+names = Hashmap()
 
 
 #for ease of operations
@@ -64,35 +70,40 @@ torch.Tensor.__mul__ = new_mul
 
 #inputs
 starting = torch.tensor([1,2,3,4], dtype=float, requires_grad=True)
+names.add(starting, "starting")
 starting2 = torch.tensor([1,5,3,5], dtype=float, requires_grad=True)
+names.add(starting2, "starting2")
 im1 = starting * starting
+names.add(im1, "im1")
 im2 = im1 * starting2
+names.add(im2, "im2")
 im3 = im2*im1
+names.add(im3, "im3")
 out = im3 * im3
-
-print(tensor_graph.edges)
-
+names.add(out, "out")
 
 
 
 
 #display
 
-fringe = [(out.grad_fn, 0)]  # pair: (function, tree display depth)
+fringe = [(out, 0)]  # pair: (function, tree display depth)
 print("--------------------------")
 #DFS (BFS will not work because tree needs to be printed out in order of depth)
 while(len(fringe) != 0):
-    current_func, current_depth = fringe.pop()
+    current_tensor, current_depth = fringe.pop()
     
 
-    if current_func.__class__.__name__ == 'AccumulateGrad':
-        print("--------" * current_depth ,"> LEAF TENSOR")
-    elif current_func == None:
-        print("--------" * current_depth ,"> LEAF TENSOR")
+    
+    if current_tensor.grad_fn.__class__.__name__ == 'AccumulateGrad' or current_tensor.grad_fn == None:
+        print("--------" * current_depth ,">", names.mapping[current_tensor], "LEAF TENSOR")
     else:
-       print("--------" * current_depth ,">",current_func)
-       for func, _ in current_func.next_functions:
-           fringe.append((func, current_depth + 1))
+        print("--------" * current_depth ,">", names.mapping[current_tensor], current_tensor.grad_fn)
+    
+    inp_tensors = tensor_graph.get_contributing_tensors(current_tensor)
+    for i in inp_tensors:
+        fringe.append((i, current_depth+1))
+    
    
         
 
